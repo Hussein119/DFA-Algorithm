1fcaaca1372e07f5bdaf77efb64a598d5a85b923
Hussein119 <hussein.abdelkader119@gmail.com>
2022-12-26 15:42:47 +0200

    Updated

--- DFA Algorithm/Source.cpp
+++ DFA Algorithm/Source.cpp
@@ -1,111 +1,212 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <map>
 using namespace std;
-void print_matrix (vector<vector<int>> v, int row, int col)
+
+class DFAMachine
 {
-	cout << "Number of rows" << v.size() << endl;
-	int i, j;
-	for (i = 0; i < row; ++i)
+public:
+	vector<string> states;
+	string alphabets;
+private:
+	vector<string> acceptStates;
+	map<string, int> statesMap;
+	map<char, int> alphabetsMap;
+	map<string, bool> isAcceptMap;
+	string startState;
+	vector<vector<string>> transitions;
+	int startStateIndex;
+
+public:
+	void setTransitions(vector<string> goingStates)
 	{
-		cout << "size of row" << v[i].size() << endl;
-		for (j = 0; j < col; ++j)
-		{
-			cout << v[i][j] << " ";
-		}
-		cout << endl;
+		transitions.push_back(goingStates);
 	}
-}
-bool accepts(vector<vector<int>> v, vector<char> sym, int row, int col, string str)
-{
-	int l, i, j, found, k, curr_state = 0;
-	l = str.length();
-	for (i = 0; i < l; ++i)
+
+	void displayMachine()
 	{
-		j = 0;
-		found = 0;
-		while (j < col && !found)
+		cout << "-> States : {";
+		for (int i = 0; i < states.size(); i++)
 		{
-			if (sym[j] == str.at(i))
+			cout << states[i];
+			if (i != states.size() - 1)
+				cout << ", ";
+		}
+		cout << "}\n\n";
+
+		cout << "-> Alphabet : {";
+		for (int i = 0; i < alphabets.size(); i++)
+		{
+			cout << alphabets[i];
+			if (i != alphabets.size() - 1)
+				cout << ", ";
+		}
+		cout << "}\n\n";
+
+		cout << "-> transition function : \n (with";
+		for (int i = 0; i < alphabets.size(); i++)
+		{
+			cout << " " << alphabets[i];
+		}
+		cout << " respectively) : \n";
+		for (int i = 0; i < transitions.size(); i++)
+		{
+			cout << "- " << states[i] << " : \t";
+			for (int j = 0; j < transitions[i].size(); j++)
 			{
-				found = 1;
+				if (transitions[i][j] == "T")
+					cout << "Phai\t";
+				else
+					cout << transitions[i][j] << "\t";
 			}
-			else
-			{
-				j++;
-			}
+			cout << endl;
 		}
-		if (j == col)
+
+		cout << "\n-> Start state : " << startState << "\n";
+
+		cout << "\n-> Accept states : {";
+		for (int i = 0; i < acceptStates.size(); i++)
 		{
-			return 0;
+
+			cout << acceptStates[i];
+			if (i != acceptStates.size() - 1)
+				cout << ", ";
 		}
-		else {
-			curr_state = v[curr_state][j];
-			if (curr_state == -1)
-			{
-				return 0;
-			}
-		}
+		cout << "}\n\n";
 	}
-	if (curr_state == row - 1)
+
+
+	void setMapStatesAndAlphabets()
 	{
-		return 1;
-	}
-	else {
-		return 0;
-	}
-}
-int main()
-{
-	int num_states, num_sym, i, j, x;
-	vector<char> sym;
-	char ch;
-	cout << "Enter number of symbols: ";
-	cin >> num_sym;
-	cout << "Enter number of states: ";
-	cin >> num_states;
-	vector<vector<int>> dfa(num_states);
-	cout << "Enter " << num_sym << " symbols for the DFA: " << endl;
-	for (i = 0; i < num_sym; ++i)
-	{
-		cin >> ch;
-		sym.push_back(ch);
-	}
-	cout << "Enter DFA matrix: " << endl;
-	for (i = 0; i < num_states; i++)
-	{
-		cout << "From q" << i << ": " << endl;
-		for (j = 0; j < num_sym; ++j)
+		for (int i = 0; i < states.size(); i++)
 		{
-			cout << "\tOn " << sym[j] << ": ";
-			cin >> x;
-			dfa[i].push_back(x);
+			statesMap[states[i]] = i;
 		}
-	}
-	print_matrix(dfa, num_states, num_sym);
-	int choice, stop = 0;
-	string str;
-	while (!stop)
-	{
-		cout << "1.check string \n2.stop\nEnter choice: ";
-		cin >> choice;
-		if (choice == 1)
+
+		for (int i = 0; i < alphabets.size(); i++)
 		{
-			cout << "Enter a string: ";
-			cin >> str;
-			x = accepts(dfa, sym, num_states, num_sym, str);
-			if (x == 1)
-			{
-				cout << "Accepted" << endl;
-			}
-			else {
-				cout << "Not accepted" << endl;
-			}
-		}
-		else {
-			stop = 1;
+			alphabetsMap[alphabets[i]] = i;
 		}
 	}
 
+	void setStartState(string startState)
+	{
+		this->startStateIndex = statesMap[startState];
+		this->startState = startState;
+	}
+
+	void setAcceptStates(vector<string> acceptStates)
+	{
+		this->acceptStates = acceptStates;
+		for (int i = 0; i < this->acceptStates.size(); i++)
+		{
+			isAcceptMap[this->acceptStates[i]] = true;
+		}
+	}
+
+	bool isAccept(string word)
+	{
+		int nextStateIndex = startStateIndex;
+		string nextState;
+		for (int i = 0; i < word.size(); i++)
+		{
+			int indexOfThisAlphabet = alphabetsMap[word[i]];
+			nextState = transitions[nextStateIndex][indexOfThisAlphabet];
+			if (nextState == "T")
+				return false;
+			nextStateIndex = statesMap[nextState];
+		}
+
+		if (isAcceptMap[nextState]) return true;
+		else return false;
+	}
+};
+
+int main()
+{
+	cout << "******************\n";
+	cout << "*DFA generic code*\n";
+	cout << "******************\n";
+	DFAMachine myMachine;
+
+	//entering the states
+	int numberOfStates;
+	cout << "-> please enter the number of states : ";
+	cin >> numberOfStates;
+	cin.ignore();
+	cout << "-----------------------------------\n";
+	vector<string> states(numberOfStates);
+	for (int i = 0; i < numberOfStates; i++)
+	{
+		cout << "   - please enter the name of state number " << i + 1 << " : ";
+		getline(cin, states[i]);
+	}
+	myMachine.states = states;
+	cout << "\n\n*******************************************************************************\n";
+	//entering the alphabets
+	string alphabets;
+	cout << "->please enter the alphabet as a one word(one string) : ";
+	getline(cin, alphabets);
+	myMachine.alphabets = alphabets;
+	cout << "\n\n*******************************************************************************\n";
+	//setting the map of the states and alphabets
+	myMachine.setMapStatesAndAlphabets();
+	//entering the transition function
+	string goingState;
+	vector<string> goingStates;
+	cout << "-> enter the transitions (enter only state name)\n";
+	cout << "------------------------------------------------\n";
+	for (int i = 0; i < states.size(); i++)
+	{
+		cout << "   - From state " << states[i] << " : \n";
+		for (int j = 0; j < alphabets.size(); j++)
+		{
+			cout << "   \t\t.on \"" << alphabets[j] << "\" (if there is none enter \"T\") : ";
+			getline(cin, goingState);
+			goingStates.push_back(goingState);
+		}
+		myMachine.setTransitions(goingStates);
+		goingStates.clear();
+	}
+	cout << "\n\n*******************************************************************************\n";
+	//entering the start state
+	string startState;
+	cout << "please enter the start state : ";
+	cin >> startState;
+	myMachine.setStartState(startState);
+	cout << "\n\n*******************************************************************************\n";
+	//entering the accept states
+	int numberOfAcceptStates;
+	cout << "please enter the number of accept states : ";
+	cin >> numberOfAcceptStates;
+	cout << "-------------------------------------------\n";
+	cin.ignore();
+	vector<string> acceptStates(numberOfAcceptStates);
+	for (int i = 0; i < numberOfAcceptStates; i++)
+	{
+		cout << "-> enter accept state number " << i + 1 << " : ";
+		getline(cin, acceptStates[i]);
+	}
+	myMachine.setAcceptStates(acceptStates);
+	cout << "\n\n***********************************************************************************************\n";
+	myMachine.displayMachine();
+	cout << "***********************************************************************************************\n\n";
+
+	cout << "->enter numbers of tries (entering words and see if it's accepted or not) : ";
+	int tries;
+	cin >> tries;
+	cout << endl;
+	while (tries--)
+	{
+		string word;
+		cout << "* enter the word : ";
+		cin >> word;
+		if (myMachine.isAccept(word))
+			cout << "ACCEPTED" << endl;
+		else
+			cout << "NOT ACCEPTED" << endl;
+	}
 
 	return 0;
 }
